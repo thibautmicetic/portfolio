9579c5368de0d47506542f59d199e3abde375e68
thibautmicetic <90472927+thibautmicetic@users.noreply.github.com>
2024-08-31 18:41:29 +0200

    pages compétences/formation/profil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, 
+    MatButtonModule, 
+    MatToolbarModule, 
+    MatIconModule, 
+    RouterModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { EducationComponent } from './education/education.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { ProfilComponent } from './profil/profil.component';
+import { SkillsComponent } from './skills/skills.component';
+
+export const routes: Routes = [
+    {path: 'education', component: EducationComponent},
+    {path: 'skills', component: SkillsComponent},
+    {path: 'experiences', component: ExperiencesComponent},
+    {path: 'profil', component: ProfilComponent},
+];
